d4ceb16db482f7c102337f0ce05f1616f35f99be
Park-seongil98 <lewis1998@naver.com>
2022-06-15 17:39:59 +0900

    get ip

--- app.js
+++ app.js
@@ -26,6 +26,23 @@ app.use(session({
       },
       name:'sesion-cookie'
 }));
+function getServerIp() {
+      var ifaces = os.networkInterfaces();
+      var result = '';
+      for (var dev in ifaces) {
+          var alias = 0;
+          ifaces[dev].forEach(function(details) {
+              if (details.family == 'IPv4' && details.internal === false) {
+                  result = details.address;
+                  ++alias;
+              }
+          });
+      }
+      return result;
+  }
+  
+  console.log(getServerIp());
+
 app.get('/', (req,res)=>{
       res.send('Hello, Express');
       console.log("app 까지 왔다");
